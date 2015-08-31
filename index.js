5aa239f3e55550b7615c9cb39cf887db06e9a0d3
Corey White <smortopahri@gmail.com>
2015-08-30 21:51:20 -0400

    Added addCoverage control

---
+++ index.js
@@ -0,0 +1,32 @@
+var FingerPrints = require('plott-mongo-models').FingerPrints,
+    wifiscanner = require('plott-wifi-scanner');
+
+/**
+ * Adds coverage point to the FingerPrints collection
+ *
+ * @module plott/addCoverage
+ * @category controller
+ * @param {body} geojson dBm RSSI dBm
+ * @return {geojson} Coveage point
+ * @example
+ * router.get('/', addCoverage);
+ *
+ * //=route
+ */
+
+
+module.exports = function (req, res) {
+  var geojson = req.body;
+  wifiscanner(function(err, aps){
+    if (err) {return handleError(res, err);}
+    geojson.properties.wifi = aps;
+    FingerPrints.create(geojson, function(err, data) {
+      if(err) { return handleError(res, err); }
+      return res.json(201, data);
+    });
+  });
+};
+
+function handleError(res, err) {
+  return res.send(500, err);
+}
