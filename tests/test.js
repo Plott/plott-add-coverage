b03f3654ab9a53cb0c10c4b6390762415ab68269
Corey White <smortopahri@gmail.com>
2015-09-05 17:09:50 -0400

    Updated docs and tests

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,29 @@
 var addCoverage = require('../'),
-    test = require('tape');
+    test = require('tape'),
+    fs = require('fs');
 
     test('addCoverage', function (t){
+      var rs = fs.createReadStream('tests/fixtures/in/scan.json');
+      var s;
+          rs.on('data', function (chunk){
+            // dataLength += chunk.length;
 
-    
+            s += chunk;
+            // console.log(typeof s)
+          });
+          rs.on('end', function () {
+            // console.log('The length was:', dataLength);
+
+            // s = JSON.parse(s);
+            console.log(s)
+            // j = JSON.parse(s)
+            // console.log(j.type)
+          })
+          rs.on('error', function (err){
+            console.log(err);
+          });
+
+
 
       t.end();
     });
